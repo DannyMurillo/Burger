1e1bc124d88851043b030632bd9d1f5d92535225
Daniel Murillo <danielmurillo@Daniels-MacBook-Pro.local>
2018-08-03 16:47:52 -0700

    schema and seeds added

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO `burgers_db`.`burgers`
+  (`burger_name`, `devoured`)
+  VALUES ('Jr. Krabby Patty', 1);
+
+INSERT INTO `burgers_db`.`burgers`
+  (`burger_name`, `devoured`)
+  VALUES ('Krabby Patty', 0);
+
+INSERT INTO `burgers_db`.'burgers'
+  (`burger_name`, `devoured`)
+  VALUES ('Double Krabby Patty', 0);
