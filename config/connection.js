0c7a74521525fe72ddc3d53b63d91081e29616cb
Daniel Murillo <danielmurillo@Daniels-MacBook-Pro.local>
2018-08-03 17:00:10 -0700

    connection.js

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+var mysql = require ('../node_modules/mysql');
+
+if (process.env.JAWSDB_URL)
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+
+    else {
+        var keys = require('./keys');
+
+        connection = mysql.createConnection({
+            host    : keys.db.host,
+            user    : keys.db.user,
+            password: keys.db.password,
+            database: keys.db.database
+        });
+    }
+
+    connection.connect(function(err) {
+        if (err) {
+            console.error('Connection error:' + err.stack);
+            return;
+        }
+        console.log('Connection threadId; ' + connection.threadId);
+    });
+
+    module.exports = connection; 
---
+++ models/burger.js
@@ -0,0 +1,7 @@
+var orm = require('../config/orm');
+
+var burger = {
+    table: 'burgers', 
+
+    allBurgers: function(cb)
+}
